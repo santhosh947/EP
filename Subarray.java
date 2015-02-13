cd9ca814643706293969d74a6d656d6723cab078
santhosh947 <santhosh.cholleti47@gmail.com>
2015-02-13 16:00:02 -0500

    with some issues

--- Subarray.java
+++ Subarray.java
@@ -3,24 +3,18 @@ public class Subarray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr={1,2,-3,4};
+		int[] arr={-1,-1,-10,6,-10};
 		
 		int max=0,prev_max=0;
-		int check_mark=0;
-		boolean add_negate=false, in_negate=false, whole_arr=false;
+		int check_mark = 0;
+		boolean  in_negate=false;
 		int start_index=0,end_index=0;
 		int best_score=0;
-		//int[] output_arr=[0];
 		ArrayList output_arr=new ArrayList();
 		ArrayList temp_arr=new ArrayList();
-//		for(int k=0;k<arr.length;k++){
-//			max+=arr[k];
-//			whole_arr=true;
-//		}
 		
        for(int i=0;i<arr.length-1;i++){
     	   max+=arr[i];
-    	//   best_score=max;
     	   temp_arr.add(arr[i]);
     	   for(int j=i+1;j<arr.length;j++){
     		   prev_max=max;
@@ -28,16 +22,13 @@ public class Subarray {
     		   if(j!=i+1){
     			   if(max>prev_max && max>best_score){     					
 	    				if(in_negate){
-	    					add_negate=true;	
 	    					for(int t=start_index;t<end_index;t++){
 	    	    				   temp_arr.add(arr[t]);
 	    	    			   }
 	    					temp_arr.add(arr[j]);
 	    					start_index=0;
 	    					end_index=0;
-	    					add_negate=false;
 	    					in_negate=false;
-	    					
 	    				}
 	    				else{
 	    					temp_arr.add(arr[j]);
@@ -62,7 +53,6 @@ public class Subarray {
     					best_score=prev_max;
     					in_negate=false;
     			   }
-    			   
     		   }
     		   else{
     		   temp_arr.add(arr[j]);
@@ -80,6 +70,7 @@ public class Subarray {
     		   check_mark=best_score;
     	   }
     	   temp_arr.clear();
+    	   max=0;
     	   
        }
        System.out.print(output_arr);
